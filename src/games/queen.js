fe6da2f08b7f7e59487e1e4b8353193b843dcd28
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-05-08 01:34:34 +0300

    remove comments

--- src/games/queen.js
+++ src/games/queen.js
@@ -3,42 +3,7 @@ import { Game } from "./gameClass";
 import "./chess.css";
 
 export class Queen_8 extends Game {
-  // validMove(state, x, y) {
-  //   if (state.board[x][y] === "♛" || state.board[x][y] === "✘") {
-  //     return false;
-  //   }
-  //   return true;
-  // }
-  // makeMove(state, x, y) {
-  //   const board = state.board;
-  //   const board2 = [
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //   ];
-  //   for (let i = 0; i < board.length; i++) {
-  //     for (let j = 0; j < board.length; j++) {
-  //       board2[i][j] = board[i][j];
-  //     }
-  //   }
 
-  //   console.log(board2);
-  //   board[x][y] = "♛";
-  //   const moves = this.queenMove(x, y);
-  //   for (let i = 0; i < moves.length; i++) {
-  //     const row = parseInt(moves[i][0]);
-  //     const col = parseInt(moves[i][1]);
-  //     if (board[row][col] === "") {
-  //       board[row][col] = "✘";
-  //     }
-  //   }
-  //   state.board = board;
-  // }
 
   controller(state, move) {
     const newState = {
@@ -135,76 +100,7 @@ export class Queen_8 extends Game {
       cells[i].innerText = board[cells[i].id.charAt(0)][cells[i].id.charAt(1)];
     }
   }
-  // controller(state, event) {
-  //   const id = event.target.id;
-  //   const x = parseInt(id[0]);
-  //   const y = parseInt(id[1]);
-  //   if (state.board[x][y] === "♛" || state.board[x][y] === "✘") {
-  //     return;
-  //   }
-  //   if (state.board[x][y] === "") {
-  //     const board = state.board;
-  //     this.stack.push(board);
-  //     board[x][y] = "♛";
-  //     const moves = this.queenMove(x, y);
-  //     for (let i = 0; i < moves.length; i++) {
-  //       const row = parseInt(moves[i][0]);
-  //       const col = parseInt(moves[i][1]);
-  //       if (board[row][col] === "") {
-  //         board[row][col] = "✘";
-  //       }
-  //     }
-  //     state.board = board;
-  //     this.drawer(state);
-  //   }
-  // }
-  // Init() {
-  //   const board = this.drawBoard(8, 8, "chess");
-  //   const board2 = [
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //     ["", "", "", "", "", "", "", ""],
-  //   ];
-  //   this.stack = [];
-  //   const clonedrows = board.props.children.map((row) => {
-  //     const clonedCells = row.props.children.map((cell) => {
-  //       return React.cloneElement(cell, {
-  //         onClick: (event) => this.controller(this.state, event),
-  //         children: board2[cell.props.id.charAt(0)][cell.props.id.charAt(1)],
-  //       });
-  //     });
-  //     return React.cloneElement(row, {}, clonedCells);
-  //   });
-  //   this.state.board = board2;
-  //   return (
-  //     <div>
-  //       <h1 style={{ textAlign: "center" }}>
-  //         8Queens
-  //         <button className="undoButton" onClick={() => this.undo(this.state)}>
-  //           undo
-  //         </button>
-  //       </h1>
-  //       <div className="chessBoard">{clonedrows}</div>
-  //     </div>
-  //   );
-  // }
-  // drawer(state) {
-  //   if (state == null) {
-  //     return this.Init();
-  //   } else {
-  //     const cells = document.getElementsByClassName("cellchess");
-
-  //     for (let i = 0; i < cells.length; i++) {
-  //       cells[i].innerText =
-  //         state.board[cells[i].id.charAt(0)][cells[i].id.charAt(1)];
-  //     }
-  //   }
-  // }
+  
   queenMove(x, y) {
     const moves = [];
     for (let i = 0; i < 8; i++) {
