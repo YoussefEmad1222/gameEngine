1806126da8d1d777bfd6f01014540df6f8d7ac07
Ahmed Medhat <96426130+Ahmed9019@users.noreply.github.com>
2023-05-08 15:12:01 +0300

    Update connect-4.js

--- src/games/connect-4.js
+++ src/games/connect-4.js
@@ -101,27 +101,6 @@ export class Connect4 extends Game {
               winner = true;
         }
       }
- 
- /*     let first = this.state.curCol + check[i]
-      let second = this.state.curCol + check[i+1]
-      let third = this.state.curCol + check[i+2]
-      let fourth = this.state.curCol + check[i+3] 
-      if( first < 0 || first > 6 ||
-          second < 0 || second > 6 ||
-          third < 0 || third > 6 ||
-          fourth < 0 || fourth > 6 ){
-          //console.log("aaaaaaaaaaaaaa row piece num in row" , this.state.curCol );
-      }else{
-        let curPiece = this.state.board[this.state.curRow][this.state.curCol];
-        //console.log(curPiece, first , second , third , fourth,"curRow",this.state.curRow,"curCol",this.state.curCol)
-        if( curPiece !== "" && board[this.state.curRow][first] === curPiece &&
-            board[this.state.curRow][second] === curPiece &&
-            board[this.state.curRow][third] === curPiece &&
-            board[this.state.curRow][fourth] === curPiece ){
-              console.log(curPiece,"asd row winner")
-              winner = true;
-        }
-      }*/
     }
 
     // check pieces in the same col
@@ -149,72 +128,9 @@ export class Connect4 extends Game {
         }
       }
 
-/*      let first = this.state.curRow + check[i]
-      let second = this.state.curRow + check[i+1]
-      let third = this.state.curRow + check[i+2]
-      let fourth = this.state.curRow + check[i+3] 
-      if( first < 0 || first > 5 ||
-          second < 0 || second > 5 ||
-          third < 0 || third > 5 ||
-          fourth < 0 || fourth > 5 ){
-          //console.log("aaaaaaaaaaaaaa col  piece num in col" , this.state.curRow );
-      }else{
-        let curPiece = this.state.board[this.state.curRow][this.state.curCol];
-        //console.log(curPiece, first , second , third , fourth,"curRow",this.state.curRow,"curCol",this.state.curCol)
-        if( curPiece !== "" && board[first][this.state.curCol] === curPiece &&
-            board[second][this.state.curCol] === curPiece &&
-            board[third][this.state.curCol] === curPiece &&
-            board[fourth][this.state.curCol] === curPiece ){
-              console.log(curPiece,"as col winner")
-              winner = true;
-        }
-      }*/
     }
 
-    // check pieces in the same diagonal \
-    /*
-    for(let i = 0 ; i < check.length - 3; i++){
-      let firstR = this.state.curRow + check[i]
-      let secondR = this.state.curRow + check[i+1]
-      let thirdR = this.state.curRow + check[i+2]
-      let fourthR = this.state.curRow + check[i+3] 
-      let firstC = this.state.curCol + check[i]
-      let secondC = this.state.curCol + check[i+1]
-      let thirdC = this.state.curCol + check[i+2]
-      let fourthC = this.state.curCol + check[i+3] 
-      if((firstR < 0 || firstR > 5 ||
-          secondR < 0 || secondR > 5 ||
-          thirdR < 0 || thirdR > 5 ||
-          fourthR < 0 || fourthR > 5)||
-        ( firstC < 0 || firstC > 6 ||
-          secondC < 0 || secondC > 6 ||
-          thirdC < 0 || thirdC > 6 ||
-          fourthC < 0 || fourthC > 6 )
-           ){
-          //console.log("aaaaaaaaaaaaaa diagonal  piece num in col" , this.state.curRow );
-      }else{
-        let curPiece = this.state.board[this.state.curRow][this.state.curCol];
-           console.log(curPiece,
-          "first index r " ,firstR ,
-          "second index r " ,secondR ,
-          "third index r " , thirdR ,
-          "fourth index r " , fourthR,"\n",
-          "first index C " ,firstC ,
-          "second index C " ,secondC ,
-          "third index C " , thirdC ,
-          "fourth index C " , fourthC,"\n",
-          "curRow",this.state.curRow,
-          "curCol",this.state.curCol
-          );
-          if( curPiece !== "" && board[firstR][firstC] === curPiece &&
-            board[secondR][secondC] === curPiece &&
-            board[thirdR][thirdC] === curPiece &&
-            board[fourthR][fourthC] === curPiece ){
-              console.log(curPiece,"as diagonal winner \\")
-              winner= true;
-        }
-      }
-    }*/
+    
 
     // check pieces in the same diagonal /
     for(let i = 0 ; i < check.length - 3; i++){
@@ -249,7 +165,7 @@ export class Connect4 extends Game {
         }
       }
     }
-    // win
+    // check pieces in the same diagonal \
     for(let i = 0 ; i < check.length - 3; i++){
       let row_arr = [];
       let col_arr = [];
